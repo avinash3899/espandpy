9f9f36ae162d03daa8e379fc2f92e254a8ea0f2b
Avinash Sinha <avinash3899@gmail.com>
2020-02-17 23:18:16 +0530

    modified to debug

--- esp_tcp_bridge/esp_tcp_bridge.ino
+++ esp_tcp_bridge/esp_tcp_bridge.ino
@@ -1,18 +1,62 @@
 #include <ESP8266WiFi.h>
-char ssid[] = "xxxxxx";        
-char pass[] = "xxxxxx";         
+class Flasher
+{
+  int ledPin;
+  long OnTime;
+  long OffTime;
+  int ledState;                
+  unsigned long previousMillis;
+  public:
+  Flasher(int pin, long on, long off)
+  {
+  ledPin = pin;
+  pinMode(ledPin, OUTPUT);     
+    
+  OnTime = on;
+  OffTime = off;
+  
+  ledState = LOW; 
+  previousMillis = 0;
+  }
+ 
+  void Update()
+  {
+    unsigned long currentMillis = millis();
+     
+    if((ledState == HIGH) && (currentMillis - previousMillis >= OnTime))
+    {
+      ledState = LOW;
+      previousMillis = currentMillis;
+      digitalWrite(ledPin, ledState);
+    }
+    else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime))
+    {
+      ledState = HIGH;
+      previousMillis = currentMillis;
+      digitalWrite(ledPin, ledState);
+    }
+  }
+};
+char ssid[] = "AvinashNetGear";        
+char pass[] = "Avinash!@#3899";         
 byte ack[20];
-IPAddress server(192,168,43,95);
+IPAddress server(192,168,1,24);
 WiFiClient client;
 long int i = 0;
+  Flasher led(4, 300, 600);
+  Flasher blue(12, 300, 600);
+  Flasher red(13, 300, 600);
+  Flasher green(16, 300, 600); 
+
 void setup() {
-  Serial.begin(115200);               
+  Serial.begin(115200);   
+         
   WiFi.begin(ssid, pass);      
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+    
+      led.Update();
   }
-   Serial.print("IP: ");     Serial.println(WiFi.localIP());
+        
 }
 
 void loop () {
@@ -24,6 +68,7 @@ if (client.connect(server,8080))
       if (client.available())
       {
         String line = client.readStringUntil('\n');
+        red.Update();
         Serial.println(line);
         while(!Serial.available())delay(5);
         String resp=Serial.readString();
